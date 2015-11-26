954690ca5e08a025855d824270818e95f58b95e8
David Guan <dguan1994@gmail.com>
2015-11-25 21:31:50 -0600

    fixed

--- js/getLore.js
+++ js/getLore.js
@@ -4,7 +4,6 @@ $('#ajax').click(function() {
 
         var key = "7fd3b97d-df0c-4ced-b7d8-810a89c8e874";
         var champName = $('#champ_name').val();  //TODO: get the champion name
-        alert(champName);
         $.ajax({
             url: 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?champData=lore&api_key=7fd3b97d-df0c-4ced-b7d8-810a89c8e874',
             type: 'GET',
